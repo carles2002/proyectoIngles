c18b2851f134dba7a1448c3e0364eb8b95c0e308
Carles <cmarfem@upv.edu.es>
2023-11-21 22:32:07 +0100

    Web Bases

---
+++ EnglishProject/EnglishProject/professor.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EnglishProject
+{
+    public partial class professor : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
