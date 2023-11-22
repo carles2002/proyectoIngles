de1540e3b436276d75d3184ee8d5107007aaacdb
Carles <cmarfem@upv.edu.es>
2023-11-22 17:40:13 +0100

    Logica de la web student aplicada

--- EnglishProject/EnglishProject/databaseController.cs
+++ EnglishProject/EnglishProject/databaseController.cs
@@ -4,6 +4,8 @@ using System.Data;
 using System.Data.SQLite;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 
 namespace EnglishProject
@@ -48,6 +50,44 @@ namespace EnglishProject
             dt.Load(reader);
 
             return dt;
+        }
+
+        //Metodo que obtiene los datos del alumno
+        public DataTable obtainStudentInfo(String DNI)
+        {
+            //Se llama al metodo para hacer consultas
+            String sentence = "SELECT * FROM students WHERE DNI = '" + DNI + "'";
+
+            DataTable dt = new DataTable();
+            dt = query(sentence);
+
+            return dt;
+        }
+        public bool updateStudentInfo(String name,String surname,String DNI, String DOB, String nationality, String address)
+        {
+            
+            bool verified = true;
+            //Se llama al metodo para hacer consultas
+            string sentence = "UPDATE students SET Name = '" + name + "', Surname = '" + surname + "', DOB = '" + DOB + "', Nationality = '" + nationality + "', Address = '" + address + "' WHERE DNI = '" + DNI + "'";
+
+            DataTable dt = new DataTable();
+            dt = query(sentence);
+            if (dt.Rows != null)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    if (name != dr["Name"].ToString()) { verified = false; }
+                    if (surname != dr["Surname"].ToString()) { verified = false; }
+                    if (DOB != dr["DOB"].ToString()) { verified = false; }
+                    if (nationality != dr["Nationality"].ToString()) { verified = false; }
+                    if (address != dr["Address"].ToString()) { verified = false; }
+                    if (DNI != dr["DNI"].ToString()) { verified = false; }
+                }
+
+            }
+            else { verified = false; }
+
+            return verified;
         }
 
 
